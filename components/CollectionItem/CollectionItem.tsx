a3087c07cbfca3e22b8ecf03498cc3bc4646c29a
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-06-26 19:23:03 +0700

    Revert "Update CollectionItem.tsx"
    This reverts commit c2915990690df7fedb4383834c3d8b6a30d54334.

--- components/CollectionItem/CollectionItem.tsx
+++ components/CollectionItem/CollectionItem.tsx
@@ -24,7 +24,6 @@ const CollectionItem = (props: Props) => {
   }
 
   if (!item) return null
-  
 
   const { onchainMetadata, externalMetadata } = item
 
@@ -38,139 +37,134 @@ const CollectionItem = (props: Props) => {
       return true
     }
 
-    // hard code filter
-  if (item.externalMetadata.symbol == 'MARMO') {
-    return (
-      <Flex
+  return (
+    <Flex
+      tabIndex={1}
+      ref={wrapperRef}
+      sx={{
+        flexDirection: "column",
+        position: "relative",
+        transition: "all .125s linear",
+        outline: "none",
+        cursor: onClick ? "pointer" : "auto",
+
+        "&:hover, &:focus, > .toggle-menu:focus": {
+          "> .toggle-menu": {
+            visibility: "visible",
+            opacity: 1,
+          },
+
+          "> img": {
+            opacity: 0.7,
+          },
+        },
+      }}
+      className={className}
+      onClick={handleOnClick(item)}
+      onKeyDown={handleKeyDown(item)}
+    >
+      <Button
         tabIndex={1}
-        ref={wrapperRef}
+        variant="resetted"
+        className="toggle-menu"
+        onClick={handleDropdownToggle}
         sx={{
-          flexDirection: "column",
-          position: "relative",
+          display: "flex",
+          position: "absolute",
+          visibility: isDropdownActive ? "visible" : "hidden",
+          opacity: isDropdownActive ? 1 : 0,
+          right: ".8rem",
+          top: ".8rem",
+          zIndex: 1,
           transition: "all .125s linear",
-          outline: "none",
-          cursor: onClick ? "pointer" : "auto",
-  
-          "&:hover, &:focus, > .toggle-menu:focus": {
-            "> .toggle-menu": {
-              visibility: "visible",
-              opacity: 1,
-            },
-  
-            "> img": {
-              opacity: 0.7,
-            },
+
+          "&:hover, &:focus": {
+            visibility: "visible",
+            cursor: "pointer",
+            opacity: 1,
           },
         }}
-        className={className}
-        onClick={handleOnClick(item)}
-        onKeyDown={handleKeyDown(item)}
       >
-        <Button
-          tabIndex={1}
-          variant="resetted"
-          className="toggle-menu"
-          onClick={handleDropdownToggle}
+        <DotsIcon
           sx={{
-            display: "flex",
-            position: "absolute",
-            visibility: isDropdownActive ? "visible" : "hidden",
-            opacity: isDropdownActive ? 1 : 0,
-            right: ".8rem",
-            top: ".8rem",
-            zIndex: 1,
-            transition: "all .125s linear",
-  
-            "&:hover, &:focus": {
-              visibility: "visible",
-              cursor: "pointer",
-              opacity: 1,
-            },
+            width: "3.2rem",
+            height: "3.2rem",
+            stroke: "heading",
+            strokeWidth: "2",
           }}
-        >
-          <DotsIcon
-            sx={{
-              width: "3.2rem",
-              height: "3.2rem",
-              stroke: "heading",
-              strokeWidth: "2",
-            }}
-          />
-        </Button>
-        {/** Dropdown */}
-        <Flex
-          sx={{
-            position: "absolute",
-            visibility: isDropdownActive ? "visible" : "hidden",
-            opacity: isDropdownActive ? 1 : 0,
-            flexDirection: "column",
-            alignItems: "flex-start",
-            padding: "2.4rem 1.2rem",
-            top: 40,
-            right: 0,
-            backgroundColor: "background",
-            transition: "all .125s linear",
-            boxShadow: "0px 4px 4px rgba(0,0,0,0.25)",
-            gap: ".8rem",
-            zIndex: 2,
-            fontSize: "1.2rem",
-  
-            a: {
-              whiteSpace: "nowrap",
-            },
-          }}
-        >
-          <a
-            href={onchainMetadata.data.uri}
-            rel="noopener noreferrer"
-            target="_blank"
-            tabIndex={1}
-          >
-            View raw JSON
-          </a>
-          <a
-            href={externalMetadata.image}
-            rel="noopener noreferrer"
-            target="_blank"
-            tabIndex={1}
-          >
-            View image
-          </a>
-          {additionalOptions || null}
-        </Flex>
-        <img
-          sx={{
-            borderRadius: ".4rem",
-            transition: "all .125s linear",
-            opacity: isDropdownActive ? 0.7 : 1,
-          }}
-          src={externalMetadata.image}
         />
-        <Text
-          variant="small"
-          sx={{
-            overflow: "hidden",
+      </Button>
+      {/** Dropdown */}
+      <Flex
+        sx={{
+          position: "absolute",
+          visibility: isDropdownActive ? "visible" : "hidden",
+          opacity: isDropdownActive ? 1 : 0,
+          flexDirection: "column",
+          alignItems: "flex-start",
+          padding: "2.4rem 1.2rem",
+          top: 40,
+          right: 0,
+          backgroundColor: "background",
+          transition: "all .125s linear",
+          boxShadow: "0px 4px 4px rgba(0,0,0,0.25)",
+          gap: ".8rem",
+          zIndex: 2,
+          fontSize: "1.2rem",
+
+          a: {
             whiteSpace: "nowrap",
-            textOverflow: "ellipsis",
-            padding: "0 .8rem",
-            mt: ".8rem",
-          }}
-        >
-          {externalMetadata.name}
-          {/* <br />
-      <a
-        href={`https://solscan.io/token/${onchainMetadata.metaData.mint}`}
-        target="_blank"
-        rel="noopener noreferrer"
+          },
+        }}
       >
-        {onchainMetadata.metaData.mint}
-      </a> */}
-        </Text>
+        <a
+          href={onchainMetadata.data.uri}
+          rel="noopener noreferrer"
+          target="_blank"
+          tabIndex={1}
+        >
+          View raw JSON
+        </a>
+        <a
+          href={externalMetadata.image}
+          rel="noopener noreferrer"
+          target="_blank"
+          tabIndex={1}
+        >
+          View image
+        </a>
+        {additionalOptions || null}
       </Flex>
-    )
-  } else {
-    return null;
-  }
+      <img
+        sx={{
+          borderRadius: ".4rem",
+          transition: "all .125s linear",
+          opacity: isDropdownActive ? 0.7 : 1,
+        }}
+        src={externalMetadata.image}
+      />
+      <Text
+        variant="small"
+        sx={{
+          overflow: "hidden",
+          whiteSpace: "nowrap",
+          textOverflow: "ellipsis",
+          padding: "0 .8rem",
+          mt: ".8rem",
+        }}
+      >
+        {externalMetadata.name}
+        {/* <br />
+    <a
+      href={`https://solscan.io/token/${onchainMetadata.metaData.mint}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {onchainMetadata.metaData.mint}
+    </a> */}
+      </Text>
+    </Flex>
+  )
 }
 
 export default CollectionItem
